6abe5ca72a3a6659b48ece8b16297d1bb0d6f5be
Aradhya Bansal <aradhyabansal2000@gmail.com>
2019-09-09 19:36:57 -0400

    added additional error handling

--- src/App.js
+++ src/App.js
@@ -98,6 +98,7 @@ displayFaceBox = (box) => {
           .then(count => {
             this.setState(Object.assign(this.state.user, { entries: count}))
           })
+          .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
